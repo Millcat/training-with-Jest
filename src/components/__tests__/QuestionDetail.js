2d0abbf7369295c3de1adac0ba117c44d3dc730e
Camille <camille.grison@gmail.com>
2020-05-25 11:31:35 +0200

    first React Redux Container Component testing - module 8

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,5 +1,26 @@
+import { mapStateToProps } from '../QuestionDetail';
+
 describe('The Question Detail Component', ()=>{
+	describe('The Container Element', () => {
+		describe('mapStateToProps', () => {
+			it('should map the state to props correctly', () => {
+				const sampleQuestion = {
+					question_id: 42,
+					body: "Space is big"
+				}
+				const appState = {
+					questions: [sampleQuestion]
+				};
+				const ownProps = {
+					question_id: 42,
+				};
+				const componentState = mapStateToProps(appState,ownProps);
+				console.log(componentState);
+				expect(componentState).toEqual(sampleQuestion);
+			});
+		});
+	});
 	it('Should not regress', ()=> {
-		expect(40 + 2).toEqual(42);
+
 	});
 });
